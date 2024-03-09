8c4b0eb9365e7dd9cf8d791458db12aa2a8daf2b
Brunotorresdev <brunotorresreal@gmail.com>
2024-03-09 08:17:12 -0300

    mudando de repo/ criacao de todas pastas e inicio da estilizacao

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# seu-orcamento
+# your-budget
+Web-site que permite calcular o orçamento para a construção de uma casa
---
+++ js/script.js
@@ -0,0 +1,25 @@
+function pageWall() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/parede/");
+  window.location.href = novoCaminho;
+}
+
+function pageLaje() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/laje/");
+  window.location.href = novoCaminho;
+}
+
+
+function pageTelhado() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/telhado/");
+  window.location.href = novoCaminho;
+}
+
+
+function pagePiso() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/piso/");
+  window.location.href = novoCaminho;
+}
